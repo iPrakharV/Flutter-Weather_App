654424280d3a7e3e8a537db1e6abb439e3f54ca8
Prakhar Verma <connect@iprakharv.com>
2024-04-10 17:00:21 -0400

    made an emply homescreen

---
+++ lib/screens/weatherpage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather App'),
+      ),
+      body: const Center(
+        child: Text('Weather Page'),
+      ),
+    );
+  }
+}
+ 
