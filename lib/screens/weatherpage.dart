c6f1347237e88cb10bf00336ab5edabcdd52e4fe
Prakhar Verma <connect@iprakharv.com>
2024-04-10 17:59:50 -0400

    Integrated Weather services in homepage

--- lib/screens/weatherpage.dart
+++ lib/screens/weatherpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_flutter_app/services/weather_services.dart';
+import 'package:weather_flutter_app/models/weather_model.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({Key? key}) : super(key: key);
@@ -8,16 +10,50 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService('ADD API KEY HERE PRAKHAR');
+  Weather? _weather;
+
+  //fetch weather data
+
+  _fetchWeather() async {
+    //get current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+//if there are errors
+    catch (e) {
+      print(e);
+    }
+  }
+  //weather animations
+
+  //init state
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather App'),
-      ),
-      body: const Center(
-        child: Text('Weather Page'),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          //city name
+          Text(_weather?.CityName ?? 'Loading City...'),
+
+          //city temperature
+          Text('${_weather?.Temperature.round()}°C'),
+        ]),
       ),
     );
   }
 }
- 
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -6,11 +6,11 @@ import 'package:geolocator/geolocator.dart';
 import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
-class WearherService {
+class WeatherService {
   static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
-  WearherService(this.apiKey);
+  WeatherService(this.apiKey);
   Future<Weather> getWeather(String cityName) async {
     final responce = await http
         .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
